b1444c04298c296fb31838ffb0efba8284ac7260
Raghav Apoorv <raghav.apoorv@gmail.com>
2019-11-08 13:08:30 -0600

    Fixed the time

--- src/main/webapp/displayTime.js
+++ src/main/webapp/displayTime.js
@@ -4,9 +4,14 @@ function getTime() {
     var xhttp = new XMLHttpRequest();
     var timeURL = "https://codeminators.herokuapp.com/api/time/";
     xhttp.onreadystatechange = function () {
+        console.log(this.status);
         if (this.readyState == 4 && this.status == 200) {
-            console.log(this.responseText);
-            setTime(new Date(this.responseText));
+            console.log(this.responseText, typeof (this.responseText));
+            // var l = new Date(this.responseText);
+            // console.log(l);
+            // setTime(new Date(this.responseText));
+            var val = this.responseText.split("T");
+            document.getElementById("time").innerHTML = val[0] + " " + val[1];
         }
     };
     xhttp.open("GET", timeURL + offset, true);
@@ -14,6 +19,8 @@ function getTime() {
 }
 
 function setTime(time) {
+    console.log(time);
+    if (!time) return;
     var current = (time.getMonth() + 1) + time.getDate() + time.getFullYear() + " " + time.getHours() + time.getMinutes();
     document.getElementById("time").innerHTML = current;
 }
