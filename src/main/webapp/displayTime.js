98dd23e2c0fe3ebf21756e4b336926b504332e39
Priyanka Rajpal <priyankarajpal0629@gmail.com>
2019-11-08 12:01:40 -0600

    Add display time script

---
+++ src/main/webapp/displayTime.js
@@ -0,0 +1,21 @@
+function getTime(){
+	var offset = new Date().getTimezoneOffset()/60;
+	console.log(offset);
+	var xhttp = new XMLHttpRequest();
+	var timeURL="https://codeminators.herokuapp.com/api/time/";
+	xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+    	console.log(this.responseText);
+      setTime(new Date(this.responseText));
+    }
+  };
+  	xhttp.open("GET", timeURL+offset, true);
+  	xhttp.send();
+}
+
+function setTime(time){
+	var current=(time.getMonth()+1)+time.getDate()+getFullYear()+" "+time.getHours()+time.getMinutes();
+	document.getElementById("time").innerHTML=current;
+}
+
+getTime();
